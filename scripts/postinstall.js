f71fb24369224d78342d458635fcb0661578ce6c
Richard Herrera <rich@doctyper.com>
2014-09-18 10:34:48 -0700

    Remove log.

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -6,8 +6,6 @@ var fs   = require("fs");
 var modernizrPath = path.join(cwd, "node_modules", "modernizr");
 
 if (fs.existsSync(modernizrPath)) {
-	console.log("Installing Modernizr dependencies");
-
 	var child = cp.spawn("npm", ["install", "--production"], {
 		cwd: modernizrPath,
 		stdio: "inherit"
