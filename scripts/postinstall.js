c9dc15613f9f7b3450ac9c4e6a6b4e86ef5c39d6
rejas <github@veeck.de>
2018-05-07 18:26:20 +0200

    Small code cleanup

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -8,7 +8,7 @@ var modernizrPath = path.join(cwd, "node_modules", "modernizr");
 
 if (fs.existsSync(modernizrPath)) {
 	console.log("--------------------------------------------------------------------------------------------------------------------------------------------");
-	console.log(("This postinstall script is a workaround for " + ("npm issue #1341: npm install should recursively check/install dependencies").bold).yellow);
+	console.log("This postinstall script is a workaround for " + ("npm issue #1341: npm install should recursively check/install dependencies").bold.yellow);
 	console.log("More info can be found here: " + ("https://github.com/npm/npm/issues/1341").cyan.underline);
 	console.log("Checking for any missing Modernizr dependencies in path: " + modernizrPath.cyan);
 	console.log("--------------------------------------------------------------------------------------------------------------------------------------------");
